c09f46d29704ccb74a1ea1ffb494cee3efe6fe19
FacuM87 <facundo.mingorance@hotmail.com>
2025-10-26 21:25:56 -0300

    estructura general

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './core/guards/auth-guard';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  { path: '', redirectTo: 'publicaciones', pathMatch: 'full' },
+  { path: 'login', loadComponent: () => import('./features/auth/login/login').then(m => m.Login) },
+  { path: 'register', loadComponent: () => import('./features/auth/register/register').then(m => m.Register) },
+  { path: 'publicaciones', loadComponent: () => import('./features/posts/publications').then(m => m.Publications), canActivate: [authGuard] },
+  { path: 'perfil', loadComponent: () => import('./features/profile/profile').then(m => m.Profile), canActivate: [authGuard] },
+  { path: '**', redirectTo: 'publicaciones' },
+];
